17782b110c18b9b269fd5dd55b23cd42249e7d64
Jared Pace <jared@codewordstudios.com>
2011-05-04 16:59:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PigstravaganzaCom::Application.config.secret_token = 'c9c9183d755273af94d4019e3cdafaebc4d1dcf690009582cc15721aab410f26bd6bf243540739864aa470a08d9b43533f0979139c523a3ffe9af109283a0ced'
