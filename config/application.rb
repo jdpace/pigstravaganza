fa7ee0ee319a1c5c2af21e896e7c194828296498
Jared Pace <jared@codewordstudios.com>
2011-05-20 14:24:43 -0400

    Initial page behaviour and layout

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module PigstravaganzaCom
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery-1.5.2.min jquery-ui-1.8.1.min railsjquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.5.2.min jquery-ui-1.8.1.min rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,66 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var Pigstravaganza = {
+
+  setup: function() {
+    Pigstravaganza.activateNavigation();
+    Pigstravaganza.activatePages();
+    Pigstravaganza.activateViewport();
+  },
+
+  activateNavigation: function() {
+    $('nav a').click(function(event) {
+      event.preventDefault();
+      var page = $( $(this).attr('href') );
+      Pigstravaganza.pages.trigger('switch', [page]);
+    });
+  },
+
+  activatePages: function() {
+    Pigstravaganza.pages = $('#pages');
+    Pigstravaganza.pages.bind('switch', this.switchPage);
+    Pigstravaganza.pages.trigger('switch', [$('.page').first()]);
+  },
+
+  switchPage: function(event, page) {
+    Pigstravaganza.scrollToPage(page);
+    Pigstravaganza.resizeContentForPage(page);
+  },
+
+  scrollToPage: function(page) {
+    var precedingPages = page.prevAll('.page');
+    var offset = Pigstravaganza.sumPageHeights(precedingPages);
+
+    console.log(offset);
+    Pigstravaganza.pages.animate({
+      top: (-1 * offset + "px")
+    });
+  },
+
+  sumPageHeights: function(pages) {
+    var sum = 0;
+
+    pages.each(function(index, page) {
+      sum += $(page).outerHeight();
+    });
+
+    return sum;
+  },
+
+  resizeContentForPage: function(page) {
+    var targetHeight = Math.max(
+      $(window).height(),
+      page.outerHeight()
+    );
+
+    $('#content').height(targetHeight);
+  },
+
+  activateViewport: function() {
+    $(window).resize(function() {
+      $('.page').css({'min-height': $(window).height() + 'px'});
+    }).resize();
+  },
+
+};
+
+
+$(Pigstravaganza.setup);
