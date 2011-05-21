06fab0d3c21a6af235946d8f260ce2aa782fb014
Alan Dipert <alan@dipert.org>
2011-05-21 15:51:29 -0400

    asdf

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module PigstravaganzaCom
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.5.2.min jquery-ui-1.8.1.min rails jquery.hoverintent underscore-min jquery.fancybox roster MD5
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.5.2.min jquery-ui-1.8.1.min rails jquery.hoverintent underscore-min jquery.fancybox roster MD5)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
